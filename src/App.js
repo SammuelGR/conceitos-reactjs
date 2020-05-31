139b3c3a40a28d07c34e71a13d8f10c44233320c
Sammuel Reis <sammugr@hotmail.com>
2020-05-31 19:49:23 -0300

    implements listing and adding static repos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState(['Repositório 1', 'Repositório 2']); 
+
   async function handleAddRepository() {
-    // TODO
+    const newRepository = 'Novo repositório';
+
+    setRepositories([...repositories, newRepository]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +18,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
+        { repositories.map(repo => (
+          <li key={repo}>
+          {repo}
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
         </li>
+        )) }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
